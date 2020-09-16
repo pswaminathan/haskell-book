5a52f5bb13f3c0f0f6ce888085bfe92f9e095d66
Prasanna Swaminathan <prasanna.swaminathan@gmail.com>
2020-09-16 08:05:52 -0400

    Add addition files

--- ch14/addition/Addition.hs
+++ ch14/addition/Addition.hs
@@ -1,11 +1,33 @@
 module Addition where
 
 import           Test.Hspec
+import           Test.QuickCheck
 
 main :: IO ()
 main = do
   testMultBy
   testDivBy
+  testAddition
+  runQc
+
+
+testAddition :: IO ()
+testAddition = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      ((1::Integer) + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      (2::Integer) + 2 `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
+
+
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
 
 
 dividedBy :: Integral a => a -> a -> (a, a)
@@ -18,9 +40,9 @@ testDivBy :: IO ()
 testDivBy = hspec $ do
   describe "divBy" $ do
     it "15 divided by 3 is 5" $ do
-      dividedBy 15 3 `shouldBe` (5, 0)
+      dividedBy (15::Integer) 3 `shouldBe` (5, 0)
     it "22 divided by 5 is 4 rem 2" $ do
-      dividedBy 22 5 `shouldBe` (4, 2)
+      dividedBy (22::Integer) 5 `shouldBe` (4, 2)
 
 
 multBy :: (Num a, Ord a) => a -> a -> a
@@ -36,15 +58,15 @@ testMultBy :: IO ()
 testMultBy = hspec $ do
   describe "multBy" $ do
     it "3 mult by 4 is 12" $ do
-      multBy 3 4 `shouldBe` 12
+      multBy (3::Integer) 4 `shouldBe` 12
     it "order of operands shouldn't matter" $ do
-      multBy 3 4 `shouldBe` (multBy 4 3)
+      multBy (3::Integer) 4 `shouldBe` (multBy 4 3)
     it "12 mult by -2 is -24" $ do
-      multBy 12 (-2) `shouldBe` (-24)
+      multBy (12::Integer) (-2) `shouldBe` (-24)
     it "-12 mult by 2 is -24" $ do
-      multBy (-12) 2 `shouldBe` (-24)
+      multBy (-12::Integer) 2 `shouldBe` (-24)
     it "negatives cancel out" $ do
-      multBy (-12) (-2) `shouldBe` 24
+      multBy (-12::Integer) (-2) `shouldBe` 24
 
 
 
---
+++ ch14/addition/GenScratch.hs
@@ -0,0 +1,66 @@
+module GenScratch where
+
+import           Test.QuickCheck
+
+-- trivial generator of values
+trivialInt :: Gen Int
+trivialInt = return 1
+
+
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1, 2, 3]
+
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+
+genTuple :: (Arbitrary a, Arbitrary b)
+         => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c)
+            => Gen (a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+
+genEither :: (Arbitrary a, Arbitrary b)
+          => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+
+-- Equal probability
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+
+-- What QuickCheck does so
+-- you get more Just values
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a))
+            ]
